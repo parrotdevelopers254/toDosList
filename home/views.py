045631bafe00079b2362f9b210dc05fbae70cc89
Robert Kamau <parrotdevelopers254@gmail.com>
2023-05-01 10:23:46 +0300

    Added an alert for successful task adding

--- home/views.py
+++ home/views.py
@@ -3,13 +3,15 @@ from home.models import Task
 
 # Create your views here.
 def home(request):
+    context = {'success': False}
     if request.method == 'POST':
         taskTitle = request.POST['title']
         taskDesc = request.POST['desc']
         ins = Task(taskTitle=taskTitle, taskDesc=taskDesc)
         ins.save()
+        context = {'success': True}
 
-    return render(request, 'index.html')
+    return render(request, 'index.html', context)
 
 def tasks(request):
     return render(request, 'tasks.html')
