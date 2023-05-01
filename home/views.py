5edbf3c0f82da66cd553c2f78a3befe2cecb8824
Robert Kamau <parrotdevelopers254@gmail.com>
2023-05-01 09:18:33 +0300

    Established models

--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render, HttpResponse
+from home.models import Task
 
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        taskTitle = request.POST['title']
+        taskDesc = request.POST['desc']
+        ins = Task(taskTitle=taskTitle, taskDesc=taskDesc)
+        ins.save()
+
     return render(request, 'index.html')
 
 def tasks(request):
